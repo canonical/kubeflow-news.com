bd808833183e78106d1206e437fbe7970d3a13f5
Robin Winslow <robin@robinwinslow.co.uk>
2019-06-25 16:43:01 +0100

    Update to flask-base 0.3.1

--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,5 @@
-import flask
+# Packages
 from canonicalwebteam.flask_base.app import FlaskBase
-
 from canonicalwebteam.templatefinder import TemplateFinder
 
 
@@ -9,18 +8,10 @@ app = FlaskBase(
     "kubeflow-news.com",
     template_folder="../templates",
     static_folder="../static",
+    template_404="404.html",
+    template_500="500.html",
 )
 
 template_finder_view = TemplateFinder.as_view("template_finder")
 app.add_url_rule("/", view_func=template_finder_view)
 app.add_url_rule("/<path:subpath>", view_func=template_finder_view)
-
-
-@app.errorhandler(404)
-def not_found_error(error):
-    return flask.render_template("404.html"), 404
-
-
-@app.errorhandler(500)
-def internal_error(error):
-    return flask.render_template("500.html"), 500
