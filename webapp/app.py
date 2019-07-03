3fc4eba25c6f941964b1a3c513adb2c2fa35c047
Ovidiu Suciu <ovidiu.suciu@canonical.com>
2019-07-03 09:24:47 +0100

    Add archives

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,6 @@
 from canonicalwebteam.blog.app import BlogExtension
 from canonicalwebteam.flask_base.app import FlaskBase
+import datetime
 
 
 app = FlaskBase(
@@ -24,3 +25,20 @@ def pluralize_filter(total_posts):
         return "s"
     else:
         return ""
+
+
+@app.template_filter('descending_years')
+def descending_years_filter(end_year):
+    now = datetime.datetime.now()
+    return range(now.year, end_year, -1)
+
+
+@app.template_filter('months_list')
+def months_list_filter(year):
+    months = []
+    now = datetime.datetime.now()
+    for i in range(1, 13):
+        date = datetime.date(year, i, 1)
+        if date < now.date():
+            months.append({"name": date.strftime("%b"), "number": i})
+    return months
