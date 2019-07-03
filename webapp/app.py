fe2b6b331345a14151c82424b0aa42490e9b52e0
Scott Mason Nash <sowasred2012@gmail.com>
2019-07-03 16:06:30 +0100

    for development, use post tag that actually has some posts associated with it

--- webapp/app.py
+++ webapp/app.py
@@ -13,5 +13,9 @@ app = FlaskBase(
 
 
 blog = BlogExtension(
-    app, "kubeflow-news.com", [3408], "kubeflow-news", "/", [3184, 3265]
+    app, "kubeflow-news.com", [2510], "ai", "/", [3184, 3265]
 )
+
+# replace line 16 with the below when
+# there are kubeflow-news posts to show:
+# app, "kubeflow-news.com", [3408], "kubeflow-news", "/", [3184, 3265]
