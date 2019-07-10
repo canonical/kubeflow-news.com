618a5df4852df81b4544c9bcb016d6f9d24d88b0
Ovidiu Suciu <40214246+solazio@users.noreply.github.com>
2019-07-10 09:34:10 +0100

    Add homepage, post page and archives page (#8)
    * add vanilla framework
    
    * add navigation
    
    * add footer
    
    * add homepage
    
    * for development, use post tag that actually has some posts associated with it
    
    * Add archives page
    
    * Add archives
    
    * Add success message on form submit

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,6 @@
 from canonicalwebteam.blog.app import BlogExtension
 from canonicalwebteam.flask_base.app import FlaskBase
+import datetime
 
 
 app = FlaskBase(
@@ -13,5 +14,36 @@ app = FlaskBase(
 
 
 blog = BlogExtension(
-    app, "kubeflow-news.com", [3408], "kubeflow-news", "/", [3184, 3265]
+    # app, "kubeflow-news.com", [3408], "kubeflow-news", "/", [3184, 3265]
+    app,
+    "kubeflow-news.com",
+    [1239],
+    "design",
+    "/",
+    [3184, 3265],
 )
+
+
+@app.template_filter("pluralize")
+def pluralize_filter(total_posts):
+    if int(total_posts) > 1:
+        return "s"
+    else:
+        return ""
+
+
+@app.template_filter("descending_years")
+def descending_years_filter(end_year):
+    now = datetime.datetime.now()
+    return range(now.year, end_year, -1)
+
+
+@app.template_filter("months_list")
+def months_list_filter(year):
+    months = []
+    now = datetime.datetime.now()
+    for i in range(1, 13):
+        date = datetime.date(year, i, 1)
+        if date < now.date():
+            months.append({"name": date.strftime("%b"), "number": i})
+    return months
